d906f53f7558e7ba05acf2b3dc90f1cf3b373915
Joselu-code-clean <71699651+Joselu-code-clean@users.noreply.github.com>
2021-11-09 13:18:46 +0100

    Make the services

--- hibernateDesafio1/src/main/java/com/example/hibernateDesafio1/services/ClienteServiceI.java
+++ hibernateDesafio1/src/main/java/com/example/hibernateDesafio1/services/ClienteServiceI.java
@@ -1,5 +1,19 @@
 package com.example.hibernateDesafio1.services;
 
+import java.util.List;
+
+import com.example.hibernateDesafio1.entities.Cliente;
+
 public interface ClienteServiceI {
 
+	public void insertar(Cliente cliente);
+	
+	public List<Cliente> getAll();
+	
+	public Cliente searchById(Long idcliente);
+	
+	public void deleted(Cliente cliente);
+
+	public void update(Cliente cliente); 
+	
 }
--- hibernateDesafio1/src/main/java/com/example/hibernateDesafio1/services/ClienteServiceImpl.java
+++ hibernateDesafio1/src/main/java/com/example/hibernateDesafio1/services/ClienteServiceImpl.java
@@ -1,5 +1,42 @@
 package com.example.hibernateDesafio1.services;
 
-public class ClienteServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.hibernateDesafio1.entities.Cliente;
+import com.example.hibernateDesafio1.entities.ClienteDaoI;
+
+public class ClienteServiceImpl implements ClienteServiceI{
+
+	@Autowired
+	private ClienteDaoI clienteDaoI;
+	
+	@Override
+	public void insertar(Cliente cliente) {
+		clienteDaoI.insertar(cliente);
+		
+	}
+
+	@Override
+	public List<Cliente> getAll() {
+		return clienteDaoI.getAll();
+	}
+
+	@Override
+	public Cliente searchById(Long idcliente) {
+		return clienteDaoI.searchById(idcliente);
+	}
+
+	@Override
+	public void deleted(Cliente cliente) {
+		clienteDaoI.deleted(cliente);
+		
+	}
+
+	@Override
+	public void update(Cliente cliente) {
+		clienteDaoI.update(cliente);
+	}
 
 }
