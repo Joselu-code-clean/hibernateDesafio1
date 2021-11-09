595c0a5bed10c6186e451eafc326645e8b47dbb5
Joselu-code-clean <71699651+Joselu-code-clean@users.noreply.github.com>
2021-11-09 13:02:41 +0100

    Completed class Clase

--- hibernateDesafio1/src/main/java/com/example/hibernateDesafio1/entities/Cliente.java
+++ hibernateDesafio1/src/main/java/com/example/hibernateDesafio1/entities/Cliente.java
@@ -1,5 +1,73 @@
 package com.example.hibernateDesafio1.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CLIENTE")
 public class Cliente {
 
+	private Long id;
+	
+	private String nombre;
+	
+	private String primer_apellido;
+	
+	private String segundo_apellido;
+	
+	private String documento_identidad;
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Column(name = "NOMBRE", nullable = false, length = 50)
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Column(name = "PRIMER_APELLIDO", nullable = false, length = 50)
+	public String getPrimer_apellido() {
+		return primer_apellido;
+	}
+
+	public void setPrimer_apellido(String primer_apellido) {
+		this.primer_apellido = primer_apellido;
+	}
+
+	@Column(name = "SEGUNDO_APELLIDO", nullable = false, length = 50)
+	public String getSegundo_apellido() {
+		return segundo_apellido;
+	}
+
+	public void setSegundo_apellido(String segundo_apellido) {
+		this.segundo_apellido = segundo_apellido;
+	}
+
+	@Column(name = "DOCUMENTO_IDENTIDAD", nullable = false, length = 9)
+	public String getDocumento_identidad() {
+		return documento_identidad;
+	}
+
+	public void setDocumento_identidad(String documento_identidad) {
+		this.documento_identidad = documento_identidad;
+	}
+	
+	
+	
 }
