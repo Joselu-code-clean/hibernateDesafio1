9b743133ce796aef0d5b8d50ae41c22d743a0d07
Joselu-code-clean <71699651+Joselu-code-clean@users.noreply.github.com>
2021-11-09 13:06:09 +0100

    Make interface ClienteDaoI

--- hibernateDesafio1/src/main/java/com/example/hibernateDesafio1/entities/ClienteDaoI.java
+++ hibernateDesafio1/src/main/java/com/example/hibernateDesafio1/entities/ClienteDaoI.java
@@ -1,5 +1,17 @@
 package com.example.hibernateDesafio1.entities;
 
+import java.util.List;
+
 public interface ClienteDaoI {
 
+	public void insertar(Cliente cliente);
+	
+	public List<Cliente> getAll();
+	
+	public Cliente searchById(Cliente cliente);
+	
+	public void deleted(Cliente cliente);
+
+	public void update(Cliente cliente);
+	
 }
